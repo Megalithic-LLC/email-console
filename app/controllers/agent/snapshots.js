95527f23039d7ab56c9fedb6fd0885b92e9bf5ed
David Rauschenbach <davidr@imap.cc>
2019-10-09 06:35:24 -0700

    Support deleting snapshots

--- app/controllers/agent/snapshots.js
+++ app/controllers/agent/snapshots.js
@@ -1,13 +1,46 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import { computed, observer } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   notify: service(),
+  selectAll: false,
+  
+  onSelectAllChanged: observer('selectAll', function() {
+    const selected = this.get('selectAll');
+    this.get('agent.snapshots').forEach((snapshot) => {
+      snapshot.set('_selected', selected);
+    });
+  }),
+  
+  onSelectedChanged: observer('agent.snapshots.@each._selected', function() {
+    const numSelected = this.get('selectedSnapshots.length');
+    const numTotal = this.get('agent.snapshots.length');
+    if (numSelected == 0) {
+      this.set('selectAll', false);
+    } else if (numSelected == numTotal) {
+      this.set('selectAll', true);
+    }
+  }),
+  
+  selectedSnapshots: computed('agent.snapshots.@each._selected', function() {
+    return this.get('agent.snapshots').filterBy('_selected',true);
+  }),
+    
   snapshotCount: computed('agent.snapshots.@each.isNew', function() {
     return this.get('agent.snapshots').filterBy('isNew', false).length;
   }),
+  
   actions: {
+    bulkDelete(snapshots) {
+      snapshots.forEach((snapshot) => {
+        snapshot.destroyRecord().then(() => {
+          this.notify.info('Snapshot deleted');
+        }).catch((reason) => {
+          this.notify.error(reason.errors[0].detail);
+        });
+      });
+    },
     takeSnapshot(agent) {
       agent.save().then(() => {
         this.notify.info('Snapshot started');
