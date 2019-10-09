72fd1c9392ab2691428fb76567a0c6746e926b78
David Rauschenbach <davidr@imap.cc>
2019-10-09 07:56:42 -0700

    Display snapshot progress

--- app/models/snapshot.js
+++ app/models/snapshot.js
@@ -4,5 +4,7 @@ const { Model } = DS;
 export default Model.extend({
   agent: DS.belongsTo('agent'),
   name: DS.attr('string'),
+  progress: DS.attr('number'),
+  size: DS.attr('number'),
   createdAt: DS.attr('date')
 });
