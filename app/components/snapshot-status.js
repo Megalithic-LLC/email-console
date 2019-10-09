6f7f42930675286677c6f4916ebd7ecb4e17ec85
David Rauschenbach <davidr@imap.cc>
2019-10-09 14:43:39 -0700

    Reload snapshot status periodically until it is fully transferred to the cloud

--- app/components/snapshot-status.js
+++ app/components/snapshot-status.js
@@ -1,4 +1,17 @@
 import Component from '@ember/component';
+import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
+  reloadModel: task(function * () {
+    let snapshot = this.get('snapshot');
+    while (true) {
+      if (snapshot.get('progress') == 100) {
+        break;
+      }
+      if (snapshot.isLoaded) {
+        snapshot.reload();
+      }
+      yield timeout(1000);
+    }
+  }).on('init').restartable()
 });
